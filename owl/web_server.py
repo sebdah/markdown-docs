066c61080bdc05009d30fe07511f5b0ef07446bd
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2013-04-25 20:28:54 +0200

    Tried to fix cache problem

--- owl/web_server.py
+++ owl/web_server.py
@@ -16,7 +16,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from flask import Flask, redirect, send_from_directory
+from flask import Flask, redirect, send_from_directory, make_response
 
 
 def run_webserver(destination_root_dir):
@@ -29,11 +29,23 @@ def run_webserver(destination_root_dir):
         destination_root_dir = destination_root_dir[:-1]
 
     app = Flask(__name__)
+
+    @app.route('/')
     @app.route('/<path:filename>')
-    def index(filename='index.html'):
+    def serve_static_html(filename='index.html'):
+        """ Serve static HTML files
+
+        :type filename: str
+        :param filename: Path to the static HTML file
+        """
         if filename.startswith(destination_root_dir):
             filename = filename.replace('{}/'.format(destination_root_dir), '')
             return redirect('/{}'.format(filename))
-        return send_from_directory('/{}'.format(destination_root_dir), filename)
+
+        response = make_response(
+            send_from_directory('/{}'.format(destination_root_dir), filename))
+        response.cache_control.no_cache = True
+
+        return response
 
     app.run()
