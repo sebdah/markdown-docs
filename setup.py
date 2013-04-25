873b85597ff1808a0b4853bb2aebcf9f178cfd18
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2013-04-25 20:00:58 +0200

    Fixed packaging scripts

---
+++ setup.py
@@ -0,0 +1,32 @@
+""" Setup script for PyPI """
+from setuptools import setup
+
+
+setup(name='owl',
+    version='0.0.3',
+    license='Apache License, Version 2.0',
+    description='Documentation generator for Markdown projects',
+    author='Sebastian Dahlgren',
+    author_email='sebastian.dahlgren@gmail.com',
+    maintainer='Sebastian Dahlgren',
+    maintainer_email='sebastian.dahlgren@gmail.com',
+    url='http://sebdah.github.com/owl/',
+    keywords="markdown documentation md mdown",
+    platforms=['Any'],
+    packages=['owl'],
+    scripts=['scripts/owl'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask >= 0.9',
+        'Jinja2 >= 2.6',
+        'Markdown >= 2.3.1',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
